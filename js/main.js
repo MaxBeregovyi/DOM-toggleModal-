291c507ee1982403bd8d75a7d19fc58af23fb0e2
MaxBeregovyi <BeregovoyMaksim777@gmail.com>
2023-12-26 20:31:31 +0200

    finish

---
+++ js/main.js
@@ -0,0 +1,30 @@
+document.querySelector(".btn").onclick = () => {
+  toggleModal("Привіт 1");
+  toggleModal(
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci fuga recusandae rerum temporibus velit voluptatum?"
+  );
+  toggleModal("Привіт 2");
+};
+
+function toggleModal(content) {
+  let existedModals = document.querySelectorAll("dialog[open]");
+  let offset = 0;
+
+  if (existedModals[0]) {
+    offset = Array.from(existedModals).reduce((acc, modal) => {
+      return acc + modal.offsetHeight + 20;
+    }, 0);
+  }
+
+  let modal = createModal(content, offset);
+  document.querySelector("body").appendChild(modal);
+}
+
+function createModal(content, top = 0) {
+  let modal = document.createElement("dialog");
+  modal.innerText = content;
+  modal.classList.add("active");
+  modal.style.top = `${top}px`;
+  modal.setAttribute("open", "");
+  return modal;
+}
